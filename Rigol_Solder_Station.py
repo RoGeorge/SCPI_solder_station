a999e8cd4d5a36a8fb2a89b57826b3507496512a
RoGeorge <george.busuioc@gmail.com>
2016-09-21 04:19:47 +0300

    Implement Rigol_Solder_Station.py script for a PID temperature control loop and charts for the text mode run.

--- Rigol_Solder_Station.py
+++ Rigol_Solder_Station.py
@@ -1,7 +1,8 @@
 __author__ = 'RoGeorge'
 #
 # TODO: Port for Linux
-# TODO: Add PID
+# TODO: Add a preheat thermocouple test
+# TODO: Add auto calibrate for PID
 # TODO: Add command line parameters for instruments IPs
 # TODO: Add GUI
 # TODO: Create versioned executable distributions
@@ -9,20 +10,44 @@ __author__ = 'RoGeorge'
 from time import *
 from sys import *
 import os
+from msvcrt import getch, kbhit
 
-from functions import connect_verify, init_oscilloscope, init_power_supply, \
-											command
-
+from functions import connect_verify, init_oscilloscope, init_power_supply, command
 
 # Update the next lines for your own default settings:
-path_to_save = ""
-save_format = "PNG"
+tSet = 250                      # Required temperature set in Celsius
+tAmb = 25
 
-IP_DS1104Z = "192.168.1.3"
-IP_DP832 = "192.168.1.4"
+# approx. 200*C at 7V without thermal load
+# infinite wait, for delta 1 Volt -> delta 20 Celsius
+# 37 seconds at 24V, from -2.82 mV to 20.9 mV (at 40uV/*C -> approx. 16*C/s for 24V)
+# measured tip temperature = 178*C at V thermocouple = 8.2mV (205*C) -> tip is 27*C lower
+# kP = 24.0 / 16.0 / 1000
 
-min_volt = float(0)
-max_volt = float(10)
+# Ziegler-Nichols method: Set kI and kD to 0.
+# Find kUltimate by increasing kP until the system oscillates.
+# pU is the oscillation period. Choose coefficients as follows:
+# +----------------------------------------------+
+# | Control Type |   kP    |    kI     |   kD    |
+# +--------------+---------+-----------+---------+
+# | P            | 0.50*kU |     0     |    0    |
+# | PI           | 0.45*kU | 1.2*kP/pU |    0    |
+# | PID          | 0.60*kU | 2.0*kP/pU | kP*pU/8 |
+# +--------------+---------+-----------+---------+
+
+# kU = 200 * 24.0 / 16.0 / 1000, fOsc=1...2 Hz
+kU = 200 * 24.0 / 16.0 / 1000
+pU = 1.5
+
+kP = 0.6 * kU
+kI = 0              # 2.0 * kP / pU
+kD = 0              # kP * pU / 8.0
+
+IP_DS1104Z = "192.168.1.3"      # Oscilloscope LAN IP address
+IP_DP832 = "192.168.1.4"        # Power Source LAN IP address
+
+vOutMin = float(0)              # Minimum voltage for the soldering iron
+vOutMax = float(24)             # Maximum voltage supported by the soldering iron
 
 # Rigol/LXI specific constants
 port = 5555
@@ -38,8 +63,12 @@ script_name = os.path.basename(argv[0])
 
 # Print usage
 print
-print "Usage:"
-print script_name
+print script_name, 'usage:'
+print '   - press UP arrow key to increase the temperature'
+print '   - press DOWN arrow key to decrease the temperature'
+print '   - press ESC key to stop the heater and exit'
+print
+print 'Initializing...'
 
 # Connect and check instruments
 tn_oscilloscope = connect_verify("oscilloscope", IP_DS1104Z, port)
@@ -51,49 +80,94 @@ init_power_supply(tn_power_source)
 
 print "Preheating..."
 # Check preheat
-voltage = 5
-command(tn_power_source, "SOURce2:VOLTage " + str(voltage))
-
-# Calibrate PID
+vOut = 0
+command(tn_power_source, "SOURce1:VOLTage " + str(vOut))
 
 # Temperature control loop
-t1 = time()
+key = 0
+previousTime = time()
+vBias = 7.0 + (tSet - 200) / 20
+iErr = 0.0
+dErr = 0.0
+pErrPrevious = 0.0
 while True:
-	# Read thermocouple
-	tn_oscilloscope.write("MEASure:ITEM? VAVG, CHANnel4")
-	buff = tn_oscilloscope.read_until("\n", small_wait)
+    # Read thermocouple
+    tn_oscilloscope.write("MEASure:ITEM? VAVG, CHANnel1")
+    buff = tn_oscilloscope.read_until("\n", small_wait)
 
-	if buff[0] < "0" or buff[0] > "9":
-		print "bing !", buff
-		buff = "0.01\n"
+    currentTime = time()
+    deltaTime = (currentTime - previousTime) * 1000     # control loop duration in ms
+    if deltaTime == 0:
+        # Prevent division by zero err
+        deltaTime = 1
 
-	# Convert thermocouple voltage readings to *C
-	t = abs(int(1000000 * float(buff[:-1]))/1000.0)
+    previousTime = currentTime
 
-	# Compute running average
+    if len(buff) > 0:
+        # print 'buff =', buff[:-1]
+        if ("0" <= buff[0] <= "9") or buff[0] == "-":
+            # Convert thermocouple voltage readings to *C
+            t = tAmb + abs(float(buff[:-1])) / 0.000040
+            # print 'T read =', t, '*C'
+        else:
+            print 'Read ERROR!', buff
+            # Will assume the current temperature is slightly higher then requested
+            t = tSet + 1
+    else:
+        print 'Empty buffer ERROR!', buff
+        # Will assume the current temperature is slightly higher then requested
 
-	# Compute next output voltage
-	if t < 10:
-		voltage += 0.1
-	if t > 10.5:
-		voltage -= 0.1
-	if voltage > 24:
-		voltage = 24
+    # Compute next output voltage to compensate for any temperature error
+    pErr = tSet - t
+    iErr += pErr * deltaTime
+    dErr = (pErr - pErrPrevious) / deltaTime
+    pErrPrevious = pErr
 
-	# Set new voltage
-	command(tn_power_source, "SOURce2:VOLTage " + "{0:.1f}".format(voltage))
+    vOut = vBias + kP * pErr + kI * iErr + kD * dErr
+    if vOut < 0:
+        vOut = 0
+    if vOut > vOutMax:
+        vOut = vOutMax
 
-	# Loop until a stop request encountered
+    # read pressed key without waiting http://code.activestate.com/recipes/197140/
+    if kbhit():                         # Key pressed?
+        key = ord(getch())              # get first byte of keyscan code
+        if key == 0 or key == 224:      # is it a function key? (0 for F1..n, 224 for arrows & others)
+            key = ord(getch())          # read second byte of key scan code
+    else:
+        key = 0
 
-	t2 = time()
-	ms_per_step = (t2 - t1) * 1000
-	t1 = t2
-	print "ms/step =", ms_per_step
+    if key == 27:   # exit if ESC pressed
+        break
+
+    if key == 72:
+        tSet += 5
+    if key == 80:
+        tSet -= 5
+
+    if tSet > 400:
+        tSet = 400
+    if tSet < 100:
+        tSet = 100
+
+    vGraph = '|.........................|'
+    vIndex = 1 + int(0.5 + vOut)
+    tGraph = '|...............................|'
+    tIndex = 1 + int((t - 100)/10)
+    # display
+    print 'vHeater =', vGraph[:vIndex] + 'v' + vGraph[vIndex + 1:], \
+          '  tSet =', tSet, '*C  ', \
+          'tTip =', tGraph[:tIndex] + 't' + tGraph[tIndex + 1:], int(t), '*C', \
+        '   deltaTime =', int(deltaTime), 'ms'
+
+    # Set new voltage
+    command(tn_power_source, "SOURce1:VOLTage " + "{0:.3f}".format(vOut))
+    # Loop until a stop request encountered
 
 # power off
-command(tn_power_source, "OUTPut CH2, OFF")
+command(tn_power_source, "OUTPut CH1, OFF")
 
 # Close telnet sessions and exit
 tn_oscilloscope.close()
 tn_power_source.close()
-print "Normal exit. Bye!"
+print "Power OFF. Normal exit. Bye!"
--- functions.py
+++ functions.py
@@ -8,86 +8,97 @@ import sys
 
 # Check network response (ping)
 def ping_IP(instrument, IP):
-	if platform.system() == "Windows":
-		response = os.system("ping -n 1 " + IP + " > nul")
-	else:
-		response = os.system("ping -c 1 " + IP + " > /dev/null")
+    if platform.system() == "Windows":
+        response = os.system("ping -n 1 " + IP + " > nul")
+    else:
+        response = os.system("ping -c 1 " + IP + " > /dev/null")
 
-	if response != 0:
-		print
-		print "No response pinging " + IP
-		print "Check network cables and settings."
-		print "You should be able to ping the " + instrument + "."
+    if response != 0:
+        print
+        print "No response pinging " + IP
+        print "Check network cables and settings."
+        print "You should be able to ping the " + instrument + "."
 
 
 # Open a telnet session for Rigol instrument
 def connect_to(instrument, IP, port):
-	tn = telnetlib.Telnet(IP, port)
-	# Ask for instrument ID
-	tn.write("*idn?")
-	instrument_id = tn.read_until("\n", 1)
+    tn = telnetlib.Telnet(IP, port)
+    # Ask for instrument ID
+    tn.write("*idn?")
+    instrument_id = tn.read_until("\n", 1)
 
-	COMPANY = 0
-	MODEL = 1
-	id_fields = instrument_id.split(",")
+    COMPANY = 0
+    MODEL = 1
+    id_fields = instrument_id.split(",")
 
-	# Check if the instrument is set to accept LAN commands
-	if id_fields[COMPANY] != "RIGOL TECHNOLOGIES":
-		print instrument_id
-		print "Non Rigol:,", instrument, "or the", instrument, "does not accept LAN commands."
-		print "Check the", instrument, "settings."
-		if instrument == "oscilloscope":
-			print "Utility -> IO Setting -> RemoteIO -> LAN must be ON"
-		if instrument == "power supply":
-			print "Utility -> IO Config -> LAN -> LAN Status must be Configured"
-		sys.exit("ERROR")
+    # Check if the instrument is set to accept LAN commands
+    if id_fields[COMPANY] != "RIGOL TECHNOLOGIES":
+        print instrument_id
+        print "Non Rigol:,", instrument, "or the", instrument, "does not accept LAN commands."
+        print "Check the", instrument, "settings."
+        if instrument == "oscilloscope":
+            print "Utility -> IO Setting -> RemoteIO -> LAN must be ON"
+        if instrument == "power supply":
+            print "Utility -> IO Config -> LAN -> LAN Status must be Configured"
+        sys.exit("ERROR")
 
-	return tn, id_fields[MODEL]
+    return tn, id_fields[MODEL]
 
 
 def connect_verify(instrument, IP, port):
-	ping_IP(instrument, IP)
-	tn, model = connect_to(instrument, IP, port)
-	if instrument == "oscilloscope" and model != "DS1104Z" or \
-		 instrument == "power supply" and model != "DP832":
-		print model, "is an unknown", instrument, "type."
-		sys.exit("ERROR")
-	return tn
+    ping_IP(instrument, IP)
+    tn, model = connect_to(instrument, IP, port)
+    if instrument == "oscilloscope" and model != "DS1104Z" or \
+                            instrument == "power supply" and model != "DP832":
+        print model, "is an unknown", instrument, "type."
+        sys.exit("ERROR")
+    return tn
 
 
 def command(tn, SCPI):
-	response = ""
-	while response != "1\n":
-		tn.write("*OPC?")                    # operation(s) completed ?
-		response = tn.read_until("\n", 1)    # wait max 1s for an answer
+    response = ""
+    while response != "1\n":
+        tn.write("*OPC?")  # operation(s) completed ?
+        response = tn.read_until("\n", 1)  # wait max 3s for an answer
 
-	tn.write(SCPI)
+    tn.write(SCPI)
 
 
 def init_oscilloscope(tn):
-	# Channel 4 ON
-	# BW Limit 20 MHz
-	# 10 mV/div
-	# POS 0
+    # General settings
+    command(tn, "RUN")                      # Run mode ON
 
-	# Trig Auto
-	# Trig Edge
-	# Trig CH4
+    # Channel 1 settings
+    command(tn, "CHANnel1:PROBe 10")
 
-	tn.write("MEASure:ITEM VAVG, CHANnel4")
-	tn.write("*opc?")                     # operation(s) completed ?
-	tn.read_until("\n", 1)                # wait max 1s for an answer
+    command(tn, "CHANnel1:BWLimit 20M")     # BW Limit 20 MHz
+    command(tn, "CHANnel1:COUPling DC")
+    command(tn, "CHANnel1:SCALe 0.01")      # 10 mV/div
+    command(tn, "CHANnel1:OFFSet 0")        # If the trace is out of range, the Vavg can not be calculated
+    command(tn, "CHANnel1:DISPlay ON")
 
-	# Run mode ON
+    # Timebase settings
+    command(tn, "TIMebase:MAIN:SCALe 0.0001")
+
+    # Trigger settings
+    command(tn, "TRIGger:SWEep AUTO")       # Trig Auto
+    command(tn, "TRIGger:EDGe:SOURce CHANnel1")
+    command(tn, "TRIGger:EDGe:LEVel 0")
+
+    # Acquisition settings
+
+    # Measurement settings
+    # command(tn, "MEASure:STATistic:RESet")
 
 
 def init_power_supply(tn):
-	command(tn, "OUTPut CH2, OFF")	          # CH2 OFF
-	command(tn, "SOURce2:VOLTage 0")          # CH2 set 0V
-	command(tn, "OUTPut:TRACk CH2, OFF")	    # CH2 NOT mirror
-	command(tn, "OUTPut:OVP:VALue CH2, 25")   # CH2 OVP limit 25 V
-	command(tn, "OUTPut:OVP CH2, ON")         # CH2 OVP on
-	command(tn, "OUTPut:OCP:VALue CH2, 3.2")  # CH2 limit 3.2A
-	command(tn, "OUTPut:OCP CH2, ON")         # CH2 OCP on
-	command(tn, "OUTPut CH2, ON")             # CH2 ON
+    command(tn, "OUTPut:TRACk CH1, OFF")        # CH2 NOT mirror
 
+    command(tn, "SOURce1:VOLTage 0")            # CH2 set 0V
+    command(tn, "SOURce1:CURRent 3")            # CH2 set 3A
+
+    command(tn, "OUTPut:OVP:VALue CH1, 25")     # CH2 OVP limit 25 V
+    command(tn, "OUTPut:OVP CH1, ON")           # CH2 OVP on
+    command(tn, "OUTPut:OCP:VALue CH1, 3.2")    # CH2 limit 3.2A
+    command(tn, "OUTPut:OCP CH1, ON")           # CH2 OCP on
+    command(tn, "OUTPut CH1, ON")               # CH2 ON
